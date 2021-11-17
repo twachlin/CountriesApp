645a79498fc8ac7f5d369197f84c6acbe5ebb139
Tomás Wachlin <56131711+twachlin@users.noreply.github.com>
2021-11-17 14:42:06 -0300

    Revert "[Feature] - Dependency Injection implemented using dagger 2"

--- app/src/main/java/curso/udemy/countriesproject/viewmodel/ListViewModel.kt
+++ app/src/main/java/curso/udemy/countriesproject/viewmodel/ListViewModel.kt
@@ -3,28 +3,21 @@ package curso.udemy.countriesproject.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import curso.udemy.countriesproject.Country
-import curso.udemy.countriesproject.di.ApiComponent
-import curso.udemy.countriesproject.di.DaggerApiComponent
 import curso.udemy.countriesproject.model.CountriesService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
-import javax.inject.Inject
 
 class ListViewModel : ViewModel() {
 
-    @Inject
-    lateinit var countriesService: CountriesService
+    private val countriesService = CountriesService()
     private val disposable = CompositeDisposable()
 
     val countries = MutableLiveData<ArrayList<Country>>()
     val countryLoadError = MutableLiveData<Boolean>()
     val loading = MutableLiveData<Boolean>()
-
-    init {
-        DaggerApiComponent.create().inject(this)
-    }
 
     fun refresh() {
         fetchCountries()
